f8f1bac3cb5607f1641b5debb5ba2582b8f6cba3
Alex Aylesbury <alex@matica.com>
2015-02-07 20:23:52 +0000

    seperating wordclock out

--- python/UnicornWordClock.py
+++ python/UnicornWordClock.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
-import UnicornGrid,fonts,time
+import UnicornGrid,fonts,time,WordClock,datetime
 
 print ("Pi Word Clock")
 grid=UnicornGrid.UnicornGrid()
 grid.bg=[0x99,0x00,0x00]
 grid.fg=[0x00,0x00,0xff]
-demoAnim=['smile','heart1','heart1F','heart2','heart2F','empty']
-grid.playAnimation(fonts.shapes,demoAnim,speed=0.1)
-grid.scrollString(fonts.textFont1," GurgleApps.com Word Clock ",speed=4)
+#demoAnim=['smile','heart1','heart1F','heart2','heart2F','empty']
+#grid.playAnimation(fonts.shapes,demoAnim,speed=0.1)
+#grid.scrollString(fonts.textFont1," GurgleApps.com Word Clock ",speed=4)
+
+wordClock=WordClock.WordClock(grid)
+while True:
+    wordClock.showTime(fonts.clockFont1,datetime.datetime.now())
---
+++ python/WordClock.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import datetime
+
+class WordClock:
+
+  myGrid=None
+
+  def __init__(self,grid):
+    print('WordClick init:')
+    self.myGrid=grid
+
+  def showTime(self,font=None,now=datetime.datetime.now()):
+    timeString='It is '
+    words=[]
+    hour=now.hour
+    minute=now.minute
+    if minute>=5 and minute <10:
+        words=['m_5']
+    elif minute>=10 and minute <15:
+        words=['m_10']
+    elif minute>=15 and minute <20:
+        words=['m_15']
+    elif minute>=20 and minute <25:
+        words=['m_20']
+    elif minute>=25 and minute <30:
+        words=['m_25']
+    elif minute>=30 and minute <35:
+        words=['m_30']
+    elif minute>=35 and minute <40:
+        words=['m_25']
+    elif minute>=40 and minute <45:
+        words=['m_20']
+    elif minute>=45 and minute <50:
+        words=['m_15']
+    elif minute>=50 and minute <55:
+        words=['m_10']
+    elif minute>=55 and minute <60:
+        words=['m_5']
+    if minute>=5 and minute<35:
+        words.append('past')
+    if minute >=35 and minute<60:
+        words.append('to')
+        hour+=1
+    if hour>12:
+        hour-=12
+    if hour==0:
+        hour=12
+    words.append('h_'+str(hour))
+    #words=['m_20','past','h_10']
+    chars=[]
+    for w in words:
+        chars.append(font[w])
+    self.myGrid.showChar(self.merge(chars))
+
+  def merge(self,words):
+    char=[0,0,0,0,0,0,0,0]
+    for w in words:
+        for x in range(0,8):
+            char[x]=char[x] | w[x]
+    return char
+
